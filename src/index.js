75be0b7ff71ce6f651c447cba9c5f0e503d543a1
cliftondavies <clifton.davies@outlook.com>
2021-02-24 21:30:57 +0000

    Initialise redux store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers/index';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
+const initialState = {
+  books: {
+    [Math.floor((Math.random() * 100))]: {
+      title: 'YDKJS',
+      category: 'Learning',
+    },
+    [Math.floor((Math.random() * 100))]: {
+      title: 'Star Wars',
+      category: 'Sci-fi',
+    },
+    [Math.floor((Math.random() * 100))]: {
+      title: 'Dunkirk',
+      category: 'History',
+    },
+  },
+};
+
+const store = createStore(rootReducer, initialState);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
