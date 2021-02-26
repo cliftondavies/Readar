1f1bd964278e25219b0c4f56ae10d76f6555feae
cliftondavies <clifton.davies@outlook.com>
2021-02-26 00:38:39 +0000

    Re-implement booksform component as stateful

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,17 +1,42 @@
-const BooksForm = () => {
+import { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { createBook } from '../actions/index';
+
+export const BooksForm = ({ createBook }) => {
+  const [book, setBook] = useState({ title: '', category: '' });
   const CATEGORIES = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-fi'];
 
+  const handleChange = e => {
+    if (e.target.tagName === 'INPUT') {
+      setBook({ title: e.target.value, category: book.category });
+    } else if (e.target.tagName === 'SELECT') {
+      setBook({ title: book.title, category: e.target.value });
+    }
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    const title = document.getElementById('title').value;
+    const category = document.getElementById('category').value;
+
+    createBook({ title, category });
+    setBook({ title: '', category: '' });
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="title">
         Title:
-        <input id="title" name="title" />
+        <input id="title" name="title" onChange={handleChange} value={book.title} required />
       </label>
       <br />
 
       <label htmlFor="category">
         Category:
-        <select name="category" id="category">
+        <select name="category" id="category" onChange={handleChange} required>
+          <option value="" disabled selected>Choose a category</option>
           <option value={CATEGORIES[0]}>{CATEGORIES[0]}</option>
           <option value={CATEGORIES[1]}>{CATEGORIES[1]}</option>
           <option value={CATEGORIES[2]}>{CATEGORIES[2]}</option>
@@ -28,4 +53,8 @@ const BooksForm = () => {
   );
 };
 
-export default BooksForm;
+BooksForm.propTypes = {
+  createBook: PropTypes.func.isRequired,
+};
+
+export default connect(null, { createBook })(BooksForm);
