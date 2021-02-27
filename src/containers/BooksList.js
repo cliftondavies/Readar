40848d89b5317c13352b1487c388d3ee13993653
cliftondavies <clifton.davies@outlook.com>
2021-02-27 01:16:18 +0000

    Implement filtering from booklist component

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,9 +1,10 @@
 import { useSelector, connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Book from '../components/Book';
-import { removeBook } from '../actions/index';
+import { removeBook, changeFilter } from '../actions/index';
+import CategoryFilter from '../components/CategoryFilter';
 
-const BooksList = ({ books, removeBook }) => {
+const BooksList = ({ books, removeBook, changeFilter }) => {
   const defaultBooks = useSelector(state => state.books);
   const booksOnDisplay = books || defaultBooks;
 
@@ -11,31 +12,52 @@ const BooksList = ({ books, removeBook }) => {
     removeBook(book);
   };
 
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Book ID</th>
-          <th>Title</th>
-          <th>Category</th>
-          <th>Remove</th>
-        </tr>
-      </thead>
+  const handleFilterChange = filter => {
+    changeFilter(filter);
+  };
 
-      <tbody>
-        {Object.keys(booksOnDisplay).map(id => (
-          <Book book={booksOnDisplay[id]} iD={id} key={id} deleteBook={handleRemoveBook} />
-        ))}
-      </tbody>
-    </table>
+  return (
+    <div>
+      <CategoryFilter filterEvent={handleFilterChange} />
+
+      <table>
+        <thead>
+          <tr>
+            <th>Book ID</th>
+            <th>Title</th>
+            <th>Category</th>
+            <th>Remove</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {Object.keys(booksOnDisplay).map(id => (
+            <Book book={booksOnDisplay[id]} iD={id} key={id} deleteBook={handleRemoveBook} />
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
-const mapStateToProps = state => ({ books: state.books });
-
-BooksList.propTypes = {
-  books: PropTypes.shape.isRequired,
-  removeBook: PropTypes.func.isRequired,
+const filterBooks = (state, filter) => {
+  const { books } = state;
+  const filteredID = Object.keys(books).filter(id => books[id].category === filter);
+  const filteredBooks = filteredID.map(id => [id, books[id]]);
+  return Object.fromEntries(filteredBooks);
 };
 
-export default connect(mapStateToProps, { removeBook })(BooksList);
+const mapStateToProps = state => {
+  const { filter } = state;
+  const books = (filter) ? filterBooks(state, filter) : state.books;
+
+  return { books, filter };
+};
+
+BooksList.propTypes = {
+  books: PropTypes.objectOf(PropTypes.object).isRequired,
+  removeBook: PropTypes.func.isRequired,
+  changeFilter: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps, { removeBook, changeFilter })(BooksList);
